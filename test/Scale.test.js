3b45e314f6bd4a595aadf0051971c2cab7011890
Alexander Cancino <ac223up@student.lnu.se>
2026-09-26 12:40:31 +0200

    Add test for minor scale

--- test/Scale.test.js
+++ test/Scale.test.js
@@ -3,10 +3,18 @@ import assert from 'node:assert'
 import { Note } from '../src/Note.js'
 import { Scale } from '../src/Scale.js'
 
-test('bulds a major scale', () => {
+test('builds a major scale', () => {
   const scale = new Scale(new Note("C4"), "major")
 
   const noteNames = scale.notes.map(note => note.name)
 
   assert.deepStrictEqual(noteNames, ["C", "D", "E", "F", "G", "A", "B"])
+})
+
+test('builds a minor scale', () => {
+  const scale = new Scale(new Note("A4"), "minor")
+
+  const noteNames = scale.notes.map(note => note.name)
+
+  assert.deepStrictEqual(noteNames, ["A", "B", "C", "D", "E", "F", "G"])
 })
